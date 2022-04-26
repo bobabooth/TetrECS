1b9c3d58ff542a037b0f5d0710be9969cf75bd5a
hadal1337 <hadal1337@pm.me>
2022-04-26 15:26:14 +0100

    Added UI to change background

--- src/main/java/uk/ac/soton/comp1206/scene/SettingsScene.java
+++ src/main/java/uk/ac/soton/comp1206/scene/SettingsScene.java
@@ -1,8 +1,10 @@
 package uk.ac.soton.comp1206.scene;
 
+import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Slider;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.text.Text;
 import org.apache.logging.log4j.LogManager;
@@ -17,6 +19,7 @@ public class SettingsScene extends BaseScene{
     private static double audioVolume = 50;
     private Slider musicSlider;
     private Slider audioSlider;
+    private static Text style = new Text("challenge-background");
 
     /**
      * Create a new settings scene
@@ -107,5 +110,76 @@ public class SettingsScene extends BaseScene{
         audioBox.getChildren().addAll(audioText, audioSlider);
 
         volumeBox.getChildren().addAll(musicBox, audioBox);
+
+        // Background editor
+        var themeGrid = new GridPane();
+        centerBox.getChildren().add(themeGrid);
+        themeGrid.setHgap(15);
+        themeGrid.setVgap(15);
+        themeGrid.setAlignment(Pos.CENTER);
+        var imagesLabel = new Text("Select Theme");
+        GridPane.setHalignment(imagesLabel, HPos.CENTER);
+        imagesLabel.getStyleClass().add("heading");
+        themeGrid.add(imagesLabel, 0, 0, 3, 1);
+
+        ImageView one = new ImageView(Multimedia.getImage("1.jpg"));
+        themeGrid.add(one, 0, 1);
+        one.setFitWidth(240);
+        one.setPreserveRatio(true);
+        one.setOnMouseClicked(e -> {
+            Multimedia.playAudio("select.mp3");
+            style = new Text("menu-background");
+            logger.info("Set theme to 1");
+        });
+
+        ImageView two = new ImageView(Multimedia.getImage("2.jpg"));
+        themeGrid.add(two, 1, 1);
+        two.setFitWidth(240);
+        two.setPreserveRatio(true);
+        two.setOnMouseClicked(e -> {
+            Multimedia.playAudio("select.mp3");
+            style = new Text("challenge-background");
+            logger.info("Set theme to 2");
+        });
+
+        ImageView three = new ImageView(Multimedia.getImage("3.jpg"));
+        themeGrid.add(three, 2, 1);
+        three.setFitWidth(240);
+        three.setPreserveRatio(true);
+        three.setOnMouseClicked(e -> {
+            Multimedia.playAudio("select.mp3");
+            style = new Text("background3");
+            logger.info("Set theme to 3");
+        });
+
+        ImageView four = new ImageView(Multimedia.getImage("4.jpg"));
+        themeGrid.add(four, 0, 2);
+        four.setFitWidth(240);
+        four.setPreserveRatio(true);
+        four.setOnMouseClicked(e -> {
+            Multimedia.playAudio("select.mp3");
+            style = new Text("background4");
+            logger.info("Set theme to 4");
+        });
+
+        ImageView five = new ImageView(Multimedia.getImage("5.jpg"));
+        themeGrid.add(five, 1, 2);
+        five.setFitWidth(240);
+        five.setPreserveRatio(true);
+        five.setOnMouseClicked(e -> {
+            Multimedia.playAudio("select.mp3");
+            style = new Text("background5");
+            logger.info("Set theme to 5");
+        });
+
+        ImageView six = new ImageView(Multimedia.getImage("6.jpg"));
+        themeGrid.add(six, 2, 2);
+        six.setFitWidth(240);
+        six.setPreserveRatio(true);
+        six.setOnMouseClicked(e -> {
+            Multimedia.playAudio("select.mp3");
+            style = new Text("background6");
+            logger.info("Set theme to 6");
+        });
     }
 }
