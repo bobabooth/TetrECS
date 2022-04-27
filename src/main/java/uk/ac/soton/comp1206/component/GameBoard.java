e14fceb25db4abc24e9ae18b21ae603539832c8f
hadal1337 <hadal1337@pm.me>
2022-04-27 19:41:32 +0100

    Decreased loading time when starting a game

--- src/main/java/uk/ac/soton/comp1206/component/GameBoard.java
+++ src/main/java/uk/ac/soton/comp1206/component/GameBoard.java
@@ -1,6 +1,7 @@
 package uk.ac.soton.comp1206.component;
 
 import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -13,10 +14,8 @@ import java.util.HashSet;
 /**
  * A GameBoard is a visual component to represent the visual GameBoard.
  * It extends a GridPane to hold a grid of GameBlocks.
- *
  * The GameBoard can hold an internal grid of its own, for example, for displaying an upcoming block. It also is
  * linked to an external grid, for the main game board.
- *
  * The GameBoard is only a visual representation and should not contain game logic or model logic in it, which should
  * take place in the Grid.
  */
@@ -33,44 +32,37 @@ public class GameBoard extends GridPane {
      * Number of rows in the board
      */
     protected final int rows;
-
-    /**
-     * The visual width of the board - has to be specified due to being a Canvas
-     */
-    private final double width;
-
-    /**
-     * The visual height of the board - has to be specified due to being a Canvas
-     */
-    private final double height;
-
     /**
      * The grid this GameBoard represents
      */
     final Grid grid;
-
+    /**
+     * The visual width of the board - has to be specified due to being a Canvas
+     */
+    private final double width;
+    /**
+     * The visual height of the board - has to be specified due to being a Canvas
+     */
+    private final double height;
     /**
      * The blocks inside the grid
      */
     public GameBlock[][] blocks;
-
+    /**
+     * The listener to call when a specific block is right-clicked
+     */
+    protected RightClickedListener rightClickedListener;
     private GameBlock hoveredBlock;
-
     /**
      * The listener to call when a specific block is clicked
      */
     private BlockClickedListener blockClickedListener;
 
-    /**
-     * The listener to call when a specific block is right-clicked
-     */
-    protected RightClickedListener rightClickedListener;
-
 
     /**
      * Create a new GameBoard, based off a given grid, with a visual width and height.
-     * @param grid linked grid
-     * @param width the visual width
+     * @param grid   linked grid
+     * @param width  the visual width
      * @param height the visual height
      */
     public GameBoard(Grid grid, double width, double height) {
@@ -87,10 +79,9 @@ public class GameBoard extends GridPane {
     /**
      * Create a new GameBoard with its own internal grid, specifying the number of columns and rows, along with the
      * visual width and height.
-     *
-     * @param cols number of columns for internal grid
-     * @param rows number of rows for internal grid
-     * @param width the visual width
+     * @param cols   number of columns for internal grid
+     * @param rows   number of rows for internal grid
+     * @param width  the visual width
      * @param height the visual height
      */
     public GameBoard(int cols, int rows, double width, double height) {
@@ -98,7 +89,7 @@ public class GameBoard extends GridPane {
         this.rows = rows;
         this.width = width;
         this.height = height;
-        this.grid = new Grid(cols,rows);
+        this.grid = new Grid(cols, rows);
 
         //Build the GameBoard
         build();
@@ -118,7 +109,7 @@ public class GameBoard extends GridPane {
      * Build the GameBoard by creating a block at every x and y column and row
      */
     protected void build() {
-        logger.info("Building grid: {} x {}",cols,rows);
+        logger.info("Building grid: {} x {}", cols, rows);
 
         setMaxWidth(width);
         setMaxHeight(height);
@@ -127,9 +118,9 @@ public class GameBoard extends GridPane {
 
         blocks = new GameBlock[cols][rows];
 
-        for(var y = 0; y < rows; y++) {
+        for (var y = 0; y < rows; y++) {
             for (var x = 0; x < cols; x++) {
-                createBlock(x,y);
+                createBlock(x, y);
             }
         }
     }
@@ -143,20 +134,22 @@ public class GameBoard extends GridPane {
         var blockWidth = width / cols;
         var blockHeight = height / rows;
 
-        //Create a new GameBlock UI component
+        // Create a new GameBlock UI component
         GameBlock block = new GameBlock(x, y, blockWidth, blockHeight);
 
-        //Add to the GridPane
+        // Add to the GridPane
         add(block, x, y);
 
-        //Add to our block directory
+        // Add to our block directory
         blocks[x][y] = block;
 
-        //Link the GameBlock component to the corresponding value in the Grid
+        // Link the GameBlock component to the corresponding value in the Grid
         block.bind(grid.getGridProperty(x, y));
 
-        //Add a mouse click handler to the block to trigger GameBoard blockClicked method
-        blockClicked(block);
+        // Add a mouse click handler to the block to trigger GameBoard blockClicked method
+        //blockClicked(block);
+        block.setOnMouseClicked((e) -> blockClicked(e, block));
+
 
         // Create hover effect when cursor is over grid
         block.setOnMouseEntered((e) -> hover(block));
@@ -166,7 +159,7 @@ public class GameBoard extends GridPane {
     }
 
     /**
-     * Hovering effect
+     * Hover effect
      */
     public void hover(GameBlock gameBlock) {
         if (this.hoveredBlock != null) {
@@ -205,20 +198,17 @@ public class GameBoard extends GridPane {
      * Triggered when a block is clicked. Call the attached listener.
      * @param block block clicked on
      */
-    private void blockClicked(GameBlock block) {
+    private void blockClicked(MouseEvent event, GameBlock block) {
         logger.info("Block clicked: {}", block);
-
-        block.setOnMouseClicked(e -> {
-            if (e.getButton().equals(MouseButton.PRIMARY)) {
-                if (blockClickedListener != null) {
-                    blockClickedListener.blockClicked(block);
-                }
+        if (event.getButton().equals(MouseButton.PRIMARY)) {
+            if (blockClickedListener != null) {
+                blockClickedListener.blockClicked(block);
             }
-            if (e.getButton().equals(MouseButton.SECONDARY)) {
-                if (rightClickedListener != null) {
-                    rightClickedListener.setOnRightClicked();
-                }
+        }
+        if (event.getButton().equals(MouseButton.SECONDARY)) {
+            if (rightClickedListener != null) {
+                rightClickedListener.setOnRightClicked();
             }
-        });
+        }
     }
 }
