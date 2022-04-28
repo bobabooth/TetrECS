292a1fc05417b29753e9fd66534eff391c7ed97c
hadal1337 <hadal1337@pm.me>
2022-04-28 10:30:57 +0100

    Misc

--- src/main/java/uk/ac/soton/comp1206/App.java
+++ src/main/java/uk/ac/soton/comp1206/App.java
@@ -2,8 +2,10 @@ package uk.ac.soton.comp1206;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
 import uk.ac.soton.comp1206.ui.GameWindow;
 
 /**
@@ -11,16 +13,18 @@ import uk.ac.soton.comp1206.ui.GameWindow;
  */
 public class App extends Application {
 
-    private static final Logger logger = LogManager.getLogger(App.class);
-    private static App instance;
     /**
      * Base resolution width
      */
     private final int width = 800;
+
     /**
      * Base resolution height
      */
     private final int height = 600;
+
+    private static App instance;
+    private static final Logger logger = LogManager.getLogger(App.class);
     private Stage stage;
 
     /**
@@ -30,14 +34,6 @@ public class App extends Application {
     public static void main(String[] args) {
         logger.info("Starting client");
         launch();
-    }
-
-    /**
-     * Get the singleton App instance
-     * @return the app
-     */
-    public static App getInstance() {
-        return instance;
     }
 
     /**
@@ -60,7 +56,7 @@ public class App extends Application {
         logger.info("Opening game window");
 
         //Change the width and height in this class to change the base rendering resolution for all game parts
-        var gameWindow = new GameWindow(stage, width, height);
+        var gameWindow = new GameWindow(stage,width,height);
 
         //Display the GameWindow
         stage.show();
@@ -74,4 +70,11 @@ public class App extends Application {
         System.exit(0);
     }
 
+    /**
+     * Get the singleton App instance
+     * @return the app
+     */
+    public static App getInstance() {
+        return instance;
+    }
 }
